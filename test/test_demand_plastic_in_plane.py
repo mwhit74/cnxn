e9ab1a36ffa7ae36e96b54de17fdf311fce110a6
mwhit74 <mwhit74@gmail.com>
2017-05-31 16:28:40 -0500

    Added additional functions to the plastic unit testing.
    Still testing. I made some changes at the end of the day and I am sure
    that not all the tests pass. Will pick up tomorrow.

--- test/test_demand_plastic_in_plane.py
+++ test/test_demand_plastic_in_plane.py
@@ -5,28 +5,36 @@ import pdb
 class TestDemandPlasticInPlane(unittest.TestCase):
 
     def setUp(self):
-        self.bolts1 = [[1,(0.0, 0.0), 1.0, [None, None], [None, None, None],
-                       [None, None], None, None, None, [None, None]],
-                      [2,(0.0, 3.0), 1.0, [None, None], [None, None, None],
-                       [None, None], None, None, None, [None, None]],
-                      [3,(0.0, 6.0), 1.0, [None, None], [None, None, None],
-                       [None, None], None, None, None, [None, None]]]
-
+        self.bolts1 = [[1,(0.0, 0.0), 1.0, [None, None], [None, None], 
+                       [None, None], [None, None], None, None, None,
+                       [None, None]],
+                       [2,(0.0, 3.0), 1.0, [None, None], [None, None], 
+                       [None, None], [None, None], None, None, None,
+                       [None, None]],
+                       [3,(0.0, 6.0), 1.0, [None, None], [None, None], 
+                       [None, None], [None, None], None, None, None,
+                       [None, None]]]
         self.force1 = [(4.0, 0.0, 0.0), (0.0, -1.0, 0.0), [None, None, None],
                         [None, None, None]]
 
-        self.bolts2 = [[1,(0.0, 0.0), 1.0, [None, None], [None, None, None],
-                       [None, None], None, None, None, [None, None]],
-                      [2,(0.0, 3.0), 1.0, [None, None], [None, None, None],
-                       [None, None], None, None, None, [None, None]],
-                      [3,(0.0, 6.0), 1.0, [None, None], [None, None, None],
-                       [None, None], None, None, None, [None, None]],
-                      [4,(6.0, 0.0), 1.0, [None, None], [None, None, None],
-                       [None, None], None, None, None, [None, None]],
-                      [5,(6.0, 3.0), 1.0, [None, None], [None, None, None],
-                       [None, None], None, None, None, [None, None]],
-                      [6,(6.0, 6.0), 1.0, [None, None], [None, None, None],
-                       [None, None], None, None, None, [None, None]]]
+        self.bolts2 = [[1,(0.0, 0.0), 1.0, [None, None], [None, None], 
+                       [None, None], [None, None], None, None, None,
+                       [None, None]],
+                       [2,(0.0, 3.0), 1.0, [None, None], [None, None], 
+                       [None, None], [None, None], None, None, None,
+                       [None, None]],
+                       [3,(0.0, 6.0), 1.0, [None, None], [None, None], 
+                       [None, None], [None, None], None, None, None,
+                       [None, None]],
+                       [4,(6.0, 0.0), 1.0, [None, None], [None, None], 
+                       [None, None], [None, None], None, None, None,
+                       [None, None]],
+                       [5,(6.0, 3.0), 1.0, [None, None], [None, None], 
+                       [None, None], [None, None], None, None, None,
+                       [None, None]],
+                       [6,(6.0, 6.0), 1.0, [None, None], [None, None], 
+                       [None, None], [None, None], None, None, None,
+                       [None, None]]]
         self.force2 = [(23.0, 8.0, 0.0), (0.6, -0.8, 0.0), [None, None, None],
                         [None, None, None]]
 
@@ -39,7 +47,7 @@ class TestDemandPlasticInPlane(unittest.TestCase):
     def test_calc_instanteous_center_1(self):
         demand.calc_local_bolt_coords(self.bolts1)
         demand.calc_local_force_coords(self.bolts1, self.force1)
-        demand.calc_moments_about_centroid(self.bolts1, self.force1)
+        demand.calc_moments_about_centroid(self.force1)
 
         cx_ic = -1.5
         cy_ic = 0.0
@@ -57,7 +65,7 @@ class TestDemandPlasticInPlane(unittest.TestCase):
     def test_calc_instanteous_center_2(self):
         demand.calc_local_bolt_coords(self.bolts2)
         demand.calc_local_force_coords(self.bolts2, self.force2)
-        demand.calc_moments_about_centroid(self.bolts2, self.force2)
+        demand.calc_moments_about_centroid(self.force2)
 
         cx_ic = -0.632 
         cy_ic = -0.474 
@@ -74,7 +82,7 @@ class TestDemandPlasticInPlane(unittest.TestCase):
     def test_calc_d_1(self):
         demand.calc_local_bolt_coords(self.bolts1)
         demand.calc_local_force_coords(self.bolts1, self.force1)
-        demand.calc_moments_about_centroid(self.bolts1, self.force1)
+        demand.calc_moments_about_centroid(self.force1)
         fx = self.force1[1][0]
         fy = self.force1[1][1]
         mo = self.force1[2][2]
@@ -85,12 +93,12 @@ class TestDemandPlasticInPlane(unittest.TestCase):
         cd  = [3.354, 1.5, 3.354]
 
         for bolt, d in zip(self.bolts1, cd):
-            self.assertAlmostEqual(bolt[6], d, places=3)
+            self.assertAlmostEqual(bolt[7], d, places=3)
 
     def test_calc_d_2(self):
         demand.calc_local_bolt_coords(self.bolts2)
         demand.calc_local_force_coords(self.bolts2, self.force2)
-        demand.calc_moments_about_centroid(self.bolts2, self.force2)
+        demand.calc_moments_about_centroid(self.force2)
         fx = self.force2[1][0]
         fy = self.force2[1][1]
         mo = self.force2[2][2]
@@ -101,15 +109,83 @@ class TestDemandPlasticInPlane(unittest.TestCase):
         cd  = [3.463,2.415,4.204,4.424,3.662,5.025]
 
         for bolt, d in zip(self.bolts2, cd):
-            self.assertAlmostEqual(bolt[6], d, places=3)
+            self.assertAlmostEqual(bolt[7], d, places=3)
 
     def test_calc_mp_1(self):
         demand.calc_local_bolt_coords(self.bolts1)
         demand.calc_local_force_coords(self.bolts1, self.force1)
-        demand.calc_moments_about_centroid(self.bolts1, self.force1)
+        demand.calc_moments_about_centroid(self.force1)
         fx = self.force1[1][0]
         fy = self.force1[1][1]
         mo = self.force1[2][2]
         x_ic, y_ic = demand.calc_instanteous_center(self.bolts1, fx, fy, mo)
 
+        mp = demand.calc_mp(self.force1, x_ic, y_ic)
+
+        c_mp = -5.5
+
+        self.assertAlmostEqual(mp, c_mp, places=3)
+
+    def test_calc_mp_2(self):
+        demand.calc_local_bolt_coords(self.bolts2)
+        demand.calc_local_force_coords(self.bolts2, self.force2)
+        demand.calc_moments_about_centroid(self.force2)
+        fx = self.force2[1][0]
+        fy = self.force2[1][1]
+        mo = self.force2[2][2]
+        x_ic, y_ic = demand.calc_instanteous_center(self.bolts2, fx, fy, mo)
+
+        mp = demand.calc_mp(self.force2, x_ic, y_ic)
+
+        c_mp = -19.789
+
+        self.assertAlmostEqual(mp, c_mp, places=3)
+
+    def test_calc_moment_about_ic_1(self):
+        demand.calc_local_bolt_coords(self.bolts1)
+        demand.calc_local_force_coords(self.bolts1, self.force1)
+        demand.calc_moments_about_centroid(self.force1)
+        fx = self.force1[1][0]
+        fy = self.force1[1][1]
+        mo = self.force1[2][2]
+        x_ic, y_ic = demand.calc_instanteous_center(self.bolts1, fx, fy, mo)
+        demand.calc_d(self.bolts1, x_ic, y_ic)
+
+        c_sum_m = 7.894
+        deltas = [0.340, 0.1521, 0.340] 
+        rs = [0.9815, 0.8731, 0.9815]
+
+        sum_m = demand.calc_moment_about_ic(self.bolts1)
+
+        self.assertAlmostEqual(c_sum_m, sum_m, places=3)
         
+        for bolt, c_delta, c_r in zip(self.bolts1, deltas, rs):
+            delta = bolt[8]
+            r = bolt[9]
+            self.assertAlmostEqual(c_delta, delta, places=3)
+            self.assertAlmostEqual(c_r, r, places=3)
+
+    def test_calc_moment_about_ic_2(self):
+        demand.calc_local_bolt_coords(self.bolts2)
+        demand.calc_local_force_coords(self.bolts2, self.force2)
+        demand.calc_moments_about_centroid(self.force2)
+        fx = self.force2[1][0]
+        fy = self.force2[1][1]
+        mo = self.force2[2][2]
+        x_ic, y_ic = demand.calc_instanteous_center(self.bolts2, fx, fy, mo)
+        demand.calc_d(self.bolts2, x_ic, y_ic)
+
+        c_sum_m = 22.210 #example gives 22.207
+        deltas = [0.234, 0.163, 0.284, 0.299, 0.248, 0.340] 
+        rs = [0.9458, 0.8870, 0.9674, 0.9720, 0.9530, 0.9815]
+
+        sum_m = demand.calc_moment_about_ic(self.bolts2)
+
+        self.assertAlmostEqual(c_sum_m, sum_m, places=3)
+        
+        for bolt, c_delta, c_r in zip(self.bolts2, deltas, rs):
+            delta = bolt[8]
+            r = bolt[9]
+            self.assertAlmostEqual(c_delta, delta, places=3)
+            self.assertAlmostEqual(c_r, r, places=3)
+
